1b42141aa9b2e0bb829a49b58812cc78dc848c02
schnee25 <schnee.2114@gmail.com>
2021-09-09 23:57:53 +0900

    why-jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 
+// class App extends Component {
+//   render() {
+//     return <div>Hello, world!</div>;
+//   }
+// }
 class App extends Component {
   render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-      </div>
-    );
+    return React.createElement("div", null, "Hello, world!!");
   }
 }
 
