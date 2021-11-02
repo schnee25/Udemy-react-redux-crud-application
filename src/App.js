1ffe0232298004d900fe00c128aafd1b55c18d14
schnee25 <schnee.2114@gmail.com>
2021-11-02 12:31:48 +0900

    learn props of component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 // import React, { Component } from "react";
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <label htmlFor="bar">bar</label>
-//         <input type="text" onChange={() => console.log("I am changed!")} />
-//       </React.Fragment>
-//     );
-//   }
-// }
 
-// 関数コンポーネント
 import React from "react";
 const App = () => {
+  const profiles = [
+    { name: "Taro", age: 10 },
+    { name: "Hanako", age: 5 },
+    { name: "Jane" }, //.defaultPropsの確認のためageなし
+  ];
   return (
     <div>
-      <Cat />
-      <Cat />
-      <Cat />
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />;
+      })}
     </div>
   );
 };
-const Cat = () => {
-  return <div>Meow!</div>;
+const User = (props) => {
+  return (
+    <div>
+      Hi! I am {props.name}, and {props.age}years old.
+    </div>
+  );
 };
+
+User.defaultProps = {
+  age: 1,
+};
+
+// const App = () => {
+//   return (
+//     <div>
+//       <User name={"Taro"} age={10} />
+//       <User name={"Hanako"} age={5} />
+//     </div>
+//   );
+// };
+// const User = (props) => {
+//   return (
+//     <div>
+//       Hi! I am {props.name} and {props.age}years old.
+//     </div>
+//   );
+// };
 
 export default App;
