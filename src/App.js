f9c80a74d6e9777651a8097986a9c4dc30e2fdc3
schnee25 <schnee.2114@gmail.com>
2021-11-02 11:59:43 +0900

    こんな書き方もできる

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from "react";
 
-// class App extends Component {
-//   render() {
-//     return <div>Hello, world!</div>;
-//   }
-// }
 class App extends Component {
   render() {
-    return React.createElement("div", null, "Hello, world!!");
+    const greeting = "Hi!!";
+    const dom = <h1>{greeting}</h1>;
+    return dom;
   }
 }
 
