aaed8caf97ad74eb24e8efa9fc6fe934198daeed
schnee25 <schnee.2114@gmail.com>
2021-11-02 12:15:34 +0900

    create class component and functionak component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => console.log("I am changed!")} />
+//       </React.Fragment>
+//     );
+//   }
+// }
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => console.log("I am changed!")} />
-      </React.Fragment>
-    );
-  }
-}
+// 関数コンポーネント
+import React from "react";
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  );
+};
+const Cat = () => {
+  return <div>Meow!</div>;
+};
 
 export default App;
